ef4b09800e6493cc76c349650ffbdb27403fb36e
Chandu <chandu.v@qount.io>
2017-03-28 11:58:09 +0530

    Search functionality

--- app/components/Search.component.ts
+++ app/components/Search.component.ts
@@ -8,9 +8,10 @@ import {Session} from "qCommon/app/services/Session";
 import {ChartOfAccountsService} from "qCommon/app/services/ChartOfAccounts.service";
 import {CompaniesService} from "qCommon/app/services/Companies.service";
 import {CustomersService} from "qCommon/app/services/Customers.service";
+import {LoadingService} from "qCommon/app/services/LoadingService";
 
-declare var jQuery:any;
-declare var _:any;
+declare let jQuery:any;
+declare let _:any;
 
 @Component({
     selector: 'search-widget',
@@ -20,31 +21,31 @@ declare var _:any;
 export class SearchComponent implements  OnInit{
     source:Array<string> = [];
     companyCurrency:string;
-
     amountCondition:string;
     textCondition:string;
     text:string;
-
     dateCondition:string;
     amount:number = 0;
     lowerLimit:number = 0;
     upperLimit:number = 0;
-
     date:string;
     beginDate:string;
     endDate:string;
-
     companyId:string;
     chartOfAccount:string;
     vendor: string;
     customer: string;
-
     chartOfAccounts:Array<any> = [];
     vendors:Array<any> = [];
     customers:Array<any> = [];
+    hasSearchResults:boolean = false;
+    showSearchCriteria:boolean = true;
+
+    tableData:any = {};
+    tableOptions:any = {};
 
     constructor(private _router: Router, private coaService: ChartOfAccountsService, private companyService: CompaniesService,
-                private customersService: CustomersService) {
+                private customersService: CustomersService, private loadingService: LoadingService) {
         this.companyCurrency=Session.getCurrentCompanyCurrency();
         this.companyId=Session.getCurrentCompany();
 
@@ -77,6 +78,11 @@ export class SearchComponent implements  OnInit{
         jQuery(document).ready(function(){
             jQuery(document).foundation();
         });
+
+        let criteria = sessionStorage.getItem("searchcriteria");
+        if(criteria){
+            this.getSearchResults(JSON.parse(criteria));
+        }
     }
 
     showPreviousPage(){
@@ -152,7 +158,77 @@ export class SearchComponent implements  OnInit{
             }
         };
         sessionStorage.setItem("searchcriteria", JSON.stringify(data));
-        console.log("data: ", data);
-        //this.resetCriteria();
+        this.getSearchResults(data);
+    }
+
+    getSearchResults(data){
+        this.loadingService.triggerLoadingEvent(true);
+        this.companyService.doSearch(data, this.companyId)
+            .subscribe(results => {
+                this.showSearchCriteria = false;
+                this.buildResultsTableData(results);
+            }, error => {
+                this.showSearchCriteria = false;
+                this.loadingService.triggerLoadingEvent(false);
+                console.log(error);
+            })
+    }
+
+    handleAction($event){
+        let link = [];
+        if($event.action == 'edit'){
+            if($event.type == 'Bill'){
+                link = ['payments/bill', this.companyId, $event.id, 'entry'];
+            } else if($event.type == 'Deposit'){
+                link = ['deposit', $event.id];
+            } else if($event.type == 'Expense'){
+                link = ['expense', $event.id];
+            } else if($event.type == 'Journal'){
+                link = ['journalEntry', $event.id];
+            } else if($event.type == 'Credit'){
+                link = ['payments/credit', this.companyId, $event.id];
+            } else if($event.type == 'Payments'){
+                link = ['payments', $event.id];
+            }
+        }
+        this._router.navigate(link);
+    }
+
+    buildResultsTableData(searchResults){
+        let base = this;
+        this.hasSearchResults = false;
+        this.tableData.rows = [];
+        this.tableOptions.search = true;
+        this.tableOptions.pageSize = 10;
+        this.tableData.columns = [
+            {"name": "type", "title": "Type"},
+            {"name": "title", "title": "Title"},
+            {"name": "amount", "title": "Amount"},
+            {"name": "date", "title": "Date"},
+            {"name": "id", "title": "Id", "visible": false},
+            {"name": "actions", "title": "", "type": "html"}
+        ];
+        _.each(searchResults, function(result){
+            let row:any = {};
+            _.each(Object.keys(result), function(key){
+                if(key == 'bill_amount' || key == 'amount'){
+                    row['amount'] = result[key]? parseFloat(result[key]).toFixed(2): '';
+                } else if(key == 'type'){
+                    row['type'] = result[key]? (result[key].charAt(0).toUpperCase() + result[key].slice(1)) : '';
+                } else if(key == 'bill_date' || key.toLowerCase() == 'date'){
+                    row['date'] = result[key];
+                } else if(key == 'title' || key.toLowerCase() == 'name' || key.toLowerCase() == 'number'){
+                    row['title'] = result[key];
+                } else{
+                    row[key] = result[key];
+                }
+            });
+            row['actions'] = "<a class='action' data-action='edit' style='margin:0px 0px 0px 5px;'><i class='icon ion-edit'></i></a>";
+            base.tableData.rows.push(row);
+        });
+        this.loadingService.triggerLoadingEvent(false);
+        setTimeout(function(){
+           base.hasSearchResults = true;
+        });
     }
 }
