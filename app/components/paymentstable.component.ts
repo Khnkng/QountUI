b6b790542c27eb56a2a6ddbc6fabe20e885032d5
Naziamirza <nazia.mirza@qount.io>
2017-04-21 17:43:35 +0530

    made changes to footable in payments table

--- app/components/paymentstable.component.ts
+++ app/components/paymentstable.component.ts
@@ -90,7 +90,7 @@ export class paymenttableComponent {
             let row:any = {};
             _.each(base.tableColumns, function(key) {
                 row[key] = expense[key];
-                row['actions'] = "<a class='action' data-action='edit' style='margin:0px 0px 0px 5px;'><i class='icon ion-edit'></i></a><a class='action' data-action='delete' style='margin:0px 0px 0px 5px;'><i class='icon ion-trash-b'></i></a>";
+                row['actions'] = "<a class='action' data-action='edit' style='margin:0px 0px 0px 5px;'><i class='icon ion-edit'></i></a>";
             });
             base.tableData.rows.push(row);
         });
