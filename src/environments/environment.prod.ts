d09c84e43bf57b9b05f4ede3195cec86f38151d2
Chandu <chandu.v@qount.io>
2017-10-13 18:14:14 +0530

    Saving footable search string and dynamic url changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  JAVA_BASE_URL: "https://services.qount.io",
+  NODE_BASE_URL: "https://services.qount.io/BigHalfNode",
+  BIGPAY_SERVICE_URL: "https://services.qount.io",
+  NOTIFICATIONS_URL: 'https://services.qount.io:8081/socket.io',
+  QOUNT_NOTIFICATIONS_URL: 'https://services.qount.io:8081/qountnotifications',
+  DWOLLA_SERVICE_URL: 'https://services.qount.io',
+  WORKFLOW_SERVICE_URL: 'https://services.qount.io',
+  DOCUMENT_SERVICE_URL: "https://services.qount.io/DocumentServices",
+  DOCHUB_SERVICE_URL: 'https://services.qount.io/DocumentServices',
+  ACTIVATION_LINK: "https://oneapp.qount.io/login"
 };
