01f8334cffa396b36f07bd4e9fb16a86d10b29f8
nagaraju91 <nagaraju.thota@qount.io>
2017-11-15 19:18:01 +0530

    documents css changes

--- src/app/components/Documents.component.ts
+++ src/app/components/Documents.component.ts
@@ -11,7 +11,7 @@ import {CompaniesService} from "qCommon/app/services/Companies.service";
 import {SwitchBoard} from "qCommon/app/services/SwitchBoard";
 import {LoadingService} from "qCommon/app/services/LoadingService";
 import {DocumentService} from "../services/Documents.service";
-
+import {pageTitleService} from "qCommon/app/services/PageTitle";
 
 declare var jQuery:any;
 declare var _:any;
@@ -23,18 +23,31 @@ declare var _:any;
 
 export class DocumentsComponent {
 
-    badges:any = [];
-    localBadges:any={};
-    routeSub:any;
-    currentCompany:any;
-    companyCurrency:string;
-    companyId:string;
+    routeSubscribe: any;
+    companyCurrency: string;
+    companyId: string;
+    documenTypeInfo: Array<any> = [{"name": "Formation Documents", "folder_display_name": "F", "color": "#44B6E8", "linkAddr": "formation"},
+      {"name": "Tax Documents", "folder_display_name": "Tx", "color": "#18457B", "linkAddr": "tax"},
+      {"name": "W2 Documents", "folder_display_name": "W2", "color": "#CD0814", "linkAddr": "w2"},
+      {"name": "I9 Documents", "folder_display_name": "I9", "color": "#808CC5", "linkAddr": "i9"},
+      {"name": "1099 Documents", "folder_display_name": "1099", "color": "#FDB844", "linkAddr": "1099"},
+      {"name": "Receipts Documents", "folder_display_name": "R", "color": "#1CB3AB", "linkAddr": "receipts"},
+      {"name": "Bills Documents", "folder_display_name": "B", "color": "#384986", "linkAddr": "bills"},
+      {"name": "Refunds Documents", "folder_display_name": "Rf", "color": "#ef655d", "linkAddr": "refunds"}
+    ];
 
-    constructor(private _router:Router,private _route: ActivatedRoute,
-                private toastService: ToastService,private switchBoard:SwitchBoard, private loadingService:LoadingService, private companiesService: CompaniesService, private documentsService:DocumentService) {
+    constructor(private _router: Router, private _route: ActivatedRoute,
+                private toastService: ToastService, private switchBoard: SwitchBoard, private loadingService: LoadingService,
+                private companiesService: CompaniesService, private documentsService: DocumentService, private titleService: pageTitleService) {
+        this.titleService.setPageTitle("Documents Types");
         this.companyId = Session.getCurrentCompany();
         this.companyCurrency = Session.getCurrentCompanyCurrency();
+      this.routeSubscribe = switchBoard.onClickPrev.subscribe(title => {
+        let link = ['tools'];
+        this._router.navigate(link);
+      });
     }
+
     goToReports(name) {
       let link = [name];
       this._router.navigate(link);
--- src/app/components/DocumentsType.component.ts
+++ src/app/components/DocumentsType.component.ts
@@ -11,10 +11,10 @@ import {CompaniesService} from "qCommon/app/services/Companies.service";
 import {SwitchBoard} from "qCommon/app/services/SwitchBoard";
 import {LoadingService} from "qCommon/app/services/LoadingService";
 import {DocumentService} from "../services/Documents.service";
+import {pageTitleService} from "qCommon/app/services/PageTitle";
 
-
-declare var jQuery:any;
-declare var _:any;
+declare var jQuery: any;
+declare var _: any;
 
 @Component({
   selector: 'employees',
@@ -22,5 +22,35 @@ declare var _:any;
 })
 
 export class DocumentsTypeComponent {
+  routeSubscribe: any;
+  companyCurrency: string;
+  companyId: string;
+
+  constructor(private _router: Router, private _route: ActivatedRoute, private toastService: ToastService, private switchBoard: SwitchBoard,
+              private loadingService: LoadingService, private companiesService: CompaniesService, private documentsService: DocumentService,
+              private titleService: pageTitleService) {
+    this.titleService.setPageTitle("Documents");
+    this.companyId = Session.getCurrentCompany();
+    this.companyCurrency = Session.getCurrentCompanyCurrency();
+
+    this.routeSubscribe = this._route.params.subscribe(params => {
+      if (params['docType'] == 'formation') {
+        console.log("Document Type =====", params['docType']);
+      } else if (params['docType'] == 'tax') {
+        console.log("Document Type =====", params['docType']);
+      } else if (params['docType'] == 'w2') {
+        console.log("Document Type =====", params['docType']);
+      }
+      else {
+        console.log("error");
+      }
+    });
+
+    this.routeSubscribe = switchBoard.onClickPrev.subscribe(title => {
+      let link = ['documents'];
+      this._router.navigate(link);
+    });
+
+  }
 
 }
